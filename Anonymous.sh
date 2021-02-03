3509e3ad298778baef4c02d5274fcf8e0e3a3928
mukesh kumar <57214574+MUKESHKUMAR2001@users.noreply.github.com>
2021-02-04 00:22:11 +0530

    manually
    manually added

---
+++ Anonymous.sh
@@ -0,0 +1,12 @@
+#Code by MukeshKumarCharak 
+#!/bin/bash
+#for (( var=0; var<100; var++ ))
+apt install espeak
+var=10
+var++
+until [ $var -lt 10 ]
+do
+    espeak -p8 -ven -s150 -k20 "Hello we are anonymous. Do not mess with us. but let me firstly. tell you who is. our god father. we do not tell their name. openly but we tell to our enymies. our god father is. mister $1. we are warning you. otherwise expect us. fuck you"
+    sleep 1s
+done
+
