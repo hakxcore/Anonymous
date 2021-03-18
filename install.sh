4eaa803e560a02767d4a5b8d00e284281947c326
MUKESHKUMAR2001 <mukeshkumarckarak@gmail.com>
2021-03-19 05:24:06 +0530

    Anonymous

---
+++ install.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+echo "Choose According to you\c"
+ehco "[1] Kali Linux"
+echo "[2] Termux"
+read Choose
+if [[ $Choose == '1']]; 
+then
+  sudo apt update && apt upgrade -y
+  sudo apt install espeak -y
+  sudo apt install pv -y
+  mv Anonymous.sh anonymous
+  chmod +x anonymous
+  echo "Installing..."
+  sleep 5
+  mv anonymous ~/bin
+  cd ..
+  rm -rf Anonymous
+  echo "Now run this command"
+  echo "anonymous <yourname>"
+fi
+
+if [[ $Choose == '2' ]]; 
+then
+  PREFIX='/data/data/com.termux/files/usr'
+  apt update && apt upgrade -y
+  apt install espeak -y
+  apt install pv -y
+  mv Anonymous.sh anonymous
+  chmod +x anonymous
+  echo "Installing..."
+  sleep 5
+  mv anonymous $PREFIX/bin
+  cd ..
+  rm -rf Anonymous
+  echo "Now run this command"
+  echo "anonymous <yourname>" 
+fi
