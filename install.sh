971b843e3f0d718d017e557e79b3d78b64360667
MUKESHKUMAR2001 <mukeshkumarckarak@gmail.com>
2021-03-19 05:31:28 +0530

    Anonymous

--- install.sh
+++ install.sh
@@ -3,16 +3,18 @@ echo "Choose According to you\c"
 ehco "[1] Kali Linux"
 echo "[2] Termux"
 read Choose
-if [[ $Choose == '1']]; 
+if [[ $Choose == '1' ]]; 
 then
   sudo apt update && apt upgrade -y
   sudo apt install espeak -y
   sudo apt install pv -y
+  echo "Removing previous file if any..."
+  rm -rf /bin/anonymous
   mv Anonymous.sh anonymous
   chmod +x anonymous
   echo "Installing..."
   sleep 5
-  mv anonymous ~/bin
+  mv anonymous /bin
   cd ..
   rm -rf Anonymous
   echo "Now run this command"
@@ -25,6 +27,8 @@ then
   apt update && apt upgrade -y
   apt install espeak -y
   apt install pv -y
+  echo "Removing previous file if any..."
+  rm -rf $PREFIX/bin/anonymous
   mv Anonymous.sh anonymous
   chmod +x anonymous
   echo "Installing..."
